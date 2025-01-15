3171b59846ee2b1439ec2f2e69a5aece751f21ca
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-01-15 19:21:59 +0100

    Add eslint

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ server.get('/ping', async (request, reply) => {
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
-    console.error(err)
+    console.error(err);
     process.exit(1)
   }
   console.log(`Server listening at ${address}`)
