37e9b88f19f26b4621f4ff43ea3c375f0ee5ce5a
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-01-15 21:44:50 +0100

    Add OpenAIEmbeddingProvider

---
+++ src/providers/embedding/embedding.provider.openai.ts
@@ -0,0 +1,28 @@
+import OpenAI from 'openai';
+import { EmbeddingProviderInterface, CreateEmbeddingInput, Embedding } from './embedding.provider.interface';
+
+type ConstructorInput = {
+  apiKey: string;
+};
+
+export class OpenAIEmbeddingProvider implements EmbeddingProviderInterface {
+  private openai: OpenAI;
+
+  constructor({ apiKey }: ConstructorInput) {
+    this.openai = new OpenAI({
+      apiKey,
+    });
+  }
+
+  async createEmbedding(input: CreateEmbeddingInput): Promise<Embedding> {
+    const response = await this.openai.embeddings.create({
+      model: 'text-embedding-3-small',
+      input: input.input,
+      encoding_format: 'float',
+    });
+
+    return {
+      embedding: response.data[0].embedding,
+    };
+  }
+}
