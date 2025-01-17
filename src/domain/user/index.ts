51001225b662508c04be1bef7b27966fc99f239f
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-01-17 21:43:12 +0100

    Add user feature entry point

---
+++ src/domain/user/index.ts
@@ -0,0 +1,5 @@
+export { User } from './model';
+export { Repository } from './repository';
+export { default as RepositorySupabase } from './repository.supabase';
+export { Service, CreateUserInput } from './service';
+export { default as ServiceImpl } from './service.impl';
