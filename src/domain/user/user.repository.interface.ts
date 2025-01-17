bd0064c7b6ecb9d4caac0daa94b4d7fbe8dce449
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-01-17 21:03:44 +0100

    Update User repo

--- src/domain/user/user.repository.interface.ts
+++ src/domain/user/user.repository.interface.ts
@@ -1,14 +1,8 @@
 import { User } from './user.model';
 
 export interface UserRepositoryInterface {
-  createUser(user: CreateUserInput): Promise<User>;
+  createUser(user: User): Promise<User>;
   findUserById(id: string): Promise<User | null>;
   updateUser(user: User): Promise<User>;
   deleteUser(id: string): Promise<void>;
 }
-
-export type CreateUserInput = {
-  id?: string;
-  name: string;
-  email: string;
-};
