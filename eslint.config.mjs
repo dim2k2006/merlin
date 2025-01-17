ab105139fd4f38abc1d6c4e7b39eb8c2f50f8388
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-01-17 22:40:51 +0100

    Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  { ignores: ['dist'] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
