0886e6b4c98b22cccc0c2e6f1761fff1d3bce26d
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-01-17 23:22:16 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,23 @@
+import { config } from 'dotenv';
 import fastify from 'fastify';
+import { UserRepositorySupabase, UserServiceImpl } from './domain/user';
+import { MemoryRepositoryPinecone, MemoryServiceImpl } from './domain/memory';
+
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const pineconeApiKey = process.env.PINECONE_API_KEY;
+
+const userRepository = new UserRepositorySupabase({ supabaseUrl, supabaseKey });
+const userService = new UserServiceImpl({ userRepository });
+
+const memoryRepository = new MemoryRepositoryPinecone({
+  apiKey: pineconeApiKey,
+  namespace: 'ns1',
+  indexName: 'merlin',
+});
+const memoryService = new MemoryServiceImpl({ memoryRepository });
 
 const server = fastify();
 
