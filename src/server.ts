4351bdd33e6ef94c0389a0977260cb851868e6ab
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-01-24 19:30:44 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,103 +1,16 @@
 import { config } from 'dotenv';
 import fastify from 'fastify';
-import { webhookCallback, Bot } from 'grammy';
-import { match } from 'ts-pattern';
-import { UserRepositorySupabase, UserServiceImpl } from './domain/user';
-import { MemoryRepositoryPinecone, MemoryServiceImpl } from './domain/memory';
-import EmbeddingProviderOpenAI from './providers/embedding/embedding.provider.openai';
-import LlmProviderOpenai from './providers/llm/llm.provider.openai';
+import { webhookCallback } from 'grammy';
+import { buildConfig, buildContainer } from './container';
+import buildBot from './bot';
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-const pineconeApiKey = process.env.PINECONE_API_KEY;
-const openaiApiKey = process.env.OPENAI_API_KEY;
-const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
+const appConfig = buildConfig();
 
-const embeddingProvider = new EmbeddingProviderOpenAI({ apiKey: openaiApiKey });
-const llmProvider = new LlmProviderOpenai({ apiKey: openaiApiKey });
+const container = buildContainer(appConfig);
 
-const userRepository = new UserRepositorySupabase({ supabaseUrl, supabaseKey });
-const userService = new UserServiceImpl({ userRepository });
-
-const memoryRepository = new MemoryRepositoryPinecone({
-  apiKey: pineconeApiKey,
-  namespace: 'ns1',
-  indexName: 'merlin',
-});
-const memoryService = new MemoryServiceImpl({ memoryRepository, embeddingProvider, llmProvider });
-
-const bot = new Bot(telegramBotToken);
-
-bot.command('start', async (ctx) => {
-  const externalId = ctx.from.id.toString();
-
-  const isUserExist = await userService.isUserExist(externalId);
-
-  if (!isUserExist) {
-    await ctx.reply('Please register first using /register command');
-
-    return;
-  }
-
-  const user = await userService.getUserByIdOrExternalId(externalId);
-
-  await ctx.reply(`Hello, ${user.firstName}! Welcome to Merlin! 🧙‍♂️`);
-});
-
-bot.command('register', async (ctx) => {
-  const externalId = ctx.from.id.toString();
-
-  const isUserExist = await userService.isUserExist(externalId);
-
-  if (isUserExist) {
-    await ctx.reply('You are already registered!');
-
-    return;
-  }
-
-  const firstName = ctx.from.first_name;
-  const lastName = ctx.from.last_name;
-
-  await userService.createUser({ externalId, firstName, lastName });
-
-  await ctx.reply('You have been successfully registered!');
-});
-
-bot.on('message', async (ctx) => {
-  const externalId = ctx.from.id.toString();
-
-  const isUserExist = await userService.isUserExist(externalId);
-
-  if (!isUserExist) {
-    await ctx.reply('Please register first using /register command');
-
-    return;
-  }
-
-  const user = await userService.getUserByIdOrExternalId(ctx.from.id.toString());
-
-  const intent = await llmProvider.identifyIntent({ message: ctx.message.text });
-
-  const action = match(intent)
-    .with('save', () => async () => {
-      await memoryService.saveMemory({ userId: user.id, content: ctx.message.text });
-
-      await ctx.react('👍');
-    })
-    .with('retrieve', () => async () => {
-      const response = await memoryService.findRelevantMemories({ userId: user.id, content: ctx.message.text, k: 50 });
-
-      await ctx.reply(response);
-    })
-    .with('unknown', () => async () => {
-      await ctx.reply('I do not understand what you are saying. 😔');
-    })
-    .exhaustive();
-
-  await action();
-});
+const bot = buildBot(container);
 
 const server = fastify();
 
