39d3baa318cc5293375f53d607435a6b0cce8da2
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-02-15 19:23:39 +0100

    Fix types

--- src/providers/llm/llm.provider.openai.ts
+++ src/providers/llm/llm.provider.openai.ts
@@ -29,7 +29,7 @@ class LlmProviderOpenai implements LlmProvider {
     });
 
     return {
-      content: response.choices[0].message.content,
+      content: response.choices[0].message.content ?? '',
     };
   }
 
